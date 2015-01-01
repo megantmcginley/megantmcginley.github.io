def8c8a11d7c28ca5293c11098540b2a90743212
megantmcginley <megantmcginley@users.noreply.github.com>
2014-12-31 21:02:09 -0500

    Update skybootV1.js

--- skybootV1.js
+++ skybootV1.js
@@ -1,7 +1,7 @@
 alert('Test');
 
 $(document).ready(function(){
-  $('#NAME', '#EMAIL').hide();
+  $('#NAME , #EMAIL').hide();
   $('#NAME').show("slide", {direction:"down"}, 1000, function(){
     $('EMAIL').show("slide", {direction:"down"}, 1000);  
   });
